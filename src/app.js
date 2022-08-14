13203069c1dbd116a175792cc224035fc9f11e07
exphoenee <bozzay.viktor@gmail.com>
2022-08-14 15:55:26 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
 "use strict";
+//import SudokuRenderer from "../SudokuRenderer/SudokuRenderer";
 
-const solver = new SudokuRenderer({ boxSizeX: 3, boxSizeY: 3 });
+const sudoku = new SudokuRenderer({ boxSizeX: 3, boxSizeY: 3 });
