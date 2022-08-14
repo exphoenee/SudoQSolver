66915d10e56f74b56d8d41e24a350d08198b4e64
exphoenee <bozzay.viktor@gmail.com>
2022-08-14 15:20:23 +0200

    Create SudokuSolver.test.js

---
+++ src/test/SudokuSolver.test.js
@@ -0,0 +1,7 @@
+"use strict";
+
+//import { SudokuSolver } from "../SudokuSolver";
+SudokuSolver = require("../SudokuSolver");
+
+console.log("It works");
+const solver = new SudokuSolver({ boxSizeX: 3, boxSizeY: 3 });
