14eecc3754019957beb2bbbd523979820f75df69
exphoenee <bozzay.viktor@gmail.com>
2022-08-14 15:31:29 +0200

    Update SudokuSolver.test.js

--- src/test/SudokuSolver.test.js
+++ src/test/SudokuSolver.test.js
@@ -1,7 +1,6 @@
 "use strict";
 
-//import { SudokuSolver } from "../SudokuSolver";
-SudokuSolver = require("../SudokuSolver");
+import SudokuSolver from "../SudokuSolver";
 
 console.log("It works");
 const solver = new SudokuSolver({ boxSizeX: 3, boxSizeY: 3 });
