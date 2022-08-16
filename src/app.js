df6e151c7e9adfaf225057788c663b826e87e0b5
exphoenee <bozzay.viktor@gmail.com>
2022-08-16 17:20:51 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
 "use strict";
-//import SudokuRenderer from "../SudokuRenderer/SudokuRenderer";
+import SudokuRenderer from "./SudokuRenderer/SudokuRenderer.js";
 
 const sudoku = new SudokuRenderer(3, 3);
