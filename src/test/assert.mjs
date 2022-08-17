785f0eb09e0c0694621a14bec577e506b0cc97d1
exphoenee <bozzay.viktor@gmail.com>
2022-08-17 22:05:32 +0200

    Create assert.mjs

---
+++ src/test/assert.mjs
@@ -0,0 +1,40 @@
+let tests = 0;
+let failed = 0;
+
+const assert = ({ caseDesc, first, check, excepted }) => {
+  tests++;
+  const tooLong = 250;
+  caseDesc = caseDesc ? `TESTCASE: ${caseDesc}\n` : "";
+  let stepText = "";
+
+  if (first) {
+    const firstResult = first();
+    const firstValue = JSON.stringify(firstResult);
+    stepText = `Called first: ${first}, returned: ${firstValue}\n`;
+  }
+
+  const exceptValue = JSON.stringify(excepted);
+  const excepText = `Exceptation is: ${
+    exceptValue.length > tooLong ? "...too long..." : exceptValue
+  }\n`;
+
+  const res = check();
+  const resultValue = JSON.stringify(res);
+  const resultText = `${first ? "Then c" : "C"}alled: ${check}, \nReturned: ${
+    resultValue && resultValue.length > tooLong ? "...too long..." : resultValue
+  }\n`;
+
+  const testResult = resultValue == exceptValue;
+
+  const decision = `Result:     ${
+    resultValue == exceptValue ? `📗ok📗\n` : `📕FAILED📕\n`
+  }`;
+  !testResult && failed++;
+
+  console.warn(
+    `----------------------TEST STEP: ${tests} Failed: ${failed}----------------`
+  );
+  console.warn(caseDesc + stepText + resultText + excepText + decision);
+};
+
+export default assert;
