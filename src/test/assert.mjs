7cc2fb66982aaa6f27215bda4b07eb6004dc0507
exphoenee <bozzay.viktor@gmail.com>
2022-08-17 22:24:49 +0200

    Update assert.mjs

--- src/test/assert.mjs
+++ src/test/assert.mjs
@@ -7,34 +7,47 @@ const assert = ({ caseDesc, first, check, excepted }) => {
   caseDesc = caseDesc ? `TESTCASE: ${caseDesc}\n` : "";
   let stepText = "";
 
-  if (first) {
-    const firstResult = first();
-    const firstValue = JSON.stringify(firstResult);
-    stepText = `Called first: ${first}, returned: ${firstValue}\n`;
+  try {
+    if (first) {
+      const firstResult = first();
+      const firstValue = JSON.stringify(firstResult);
+      stepText = `Called first: ${first}, returned: ${firstValue}\n`;
+    }
+
+    const exceptValue = JSON.stringify(excepted);
+    const excepText = `Exceptation is: ${
+      exceptValue.length > tooLong ? "...too long..." : exceptValue
+    }\n`;
+
+    const res = check();
+    const resultValue = JSON.stringify(res);
+    const resultText = `${first ? "Then c" : "C"}alled: ${check}, \nReturned: ${
+      resultValue && resultValue.length > tooLong
+        ? "...too long..."
+        : resultValue
+    }\n`;
+
+    const testResult = resultValue == exceptValue;
+
+    const decision = `Result:     ${
+      resultValue == exceptValue ? `📗ok📗\n` : `📕FAILED📕\n`
+    }`;
+    !testResult && failed++;
+
+    console.warn(
+      `----------------------TEST STEP: ${tests} Failed: ${failed}----------------`
+    );
+    console.warn(caseDesc + stepText + resultText + excepText + decision);
+  } catch {
+    failed++;
+    console.warn(
+      `----------------------TEST STEP: ${tests} Failed: ${failed}----------------`
+    );
+    console.warn("Something went wrong with test case: " + caseDesc);
+    console.warn(
+      `${first ? `Called first: ${first}, then c` : "C"}alled: ${check}`
+    );
   }
-
-  const exceptValue = JSON.stringify(excepted);
-  const excepText = `Exceptation is: ${
-    exceptValue.length > tooLong ? "...too long..." : exceptValue
-  }\n`;
-
-  const res = check();
-  const resultValue = JSON.stringify(res);
-  const resultText = `${first ? "Then c" : "C"}alled: ${check}, \nReturned: ${
-    resultValue && resultValue.length > tooLong ? "...too long..." : resultValue
-  }\n`;
-
-  const testResult = resultValue == exceptValue;
-
-  const decision = `Result:     ${
-    resultValue == exceptValue ? `📗ok📗\n` : `📕FAILED📕\n`
-  }`;
-  !testResult && failed++;
-
-  console.warn(
-    `----------------------TEST STEP: ${tests} Failed: ${failed}----------------`
-  );
-  console.warn(caseDesc + stepText + resultText + excepText + decision);
 };
 
 export default assert;
