45b091f91dd69b2050b3223b3df2ee249c683b48
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-08-23 13:30:21 +0200

    Update SudokuGenerator.mjs

--- src/SudokuGenerator/SudokuGenerator.mjs
+++ src/SudokuGenerator/SudokuGenerator.mjs
@@ -1,16 +1,21 @@
 "use strict";
 
+import SudokuSolver from "../SudokuGenerator/SudokuSolver.mjs";
+
 export default class SudokuGenerator {
   #sudokuboard;
   #boxSizeX;
   #boxSizeY;
   #puzzle;
+  #solver;
 
   constructor(sudokuboard) {
     //the size of a section and matrix of sections n x n, but the css isn't made for other sizes only 3 x 3 sudokus...
     this.#boxSizeX = sudokuboard.boardSize.boxSizeX;
     this.#boxSizeY = sudokuboard.boardSize.boxSizeY;
     this.#puzzle = sudokuboard.getCellValues({ format: "2D" });
+
+    this.#solver = new SudokuSolver(sudokuboard);
 
     //using the #sudokuboard calss for handling the sudoku board
     this.#sudokuboard = sudokuboard;
@@ -42,15 +47,36 @@ export default class SudokuGenerator {
     return this.#sudokuboard.getCellPossiblities(cell);
   }
 
-  getARandomFreeCell() {
+  getRandomFreeCell() {
     const freeCells = this.getFreeCells();
     return freeCells[Math.floor(Math.random() * freeCells.length)];
   }
 
-  setARandomCellToRandomValue() {
-    const cell = this.getARandomFreeCell();
-    const possibleities = this.getCellPossiblities(cell);
-    const value = Math.floor(Math.random() * possibleities.length);
-    cell.setValue(possibleities[value]);
+  setCellRandomValue(cell) {
+    const possibleities = this.getCellPossiblities({ cell });
+    const value =
+      possibleities[Math.floor(Math.random() * possibleities.length)];
+    if (value) cell.setValue(+value);
+  }
+
+  setRandomCellToRandomValue() {
+    const cell = this.getRandomFreeCell();
+    this.setCellRandomValue(cell);
+  }
+
+  generateBoard({ level } = { level: "easy" }) {
+    const cellAmmount = {
+      easy: 0.5,
+      medium: 0.6,
+      hard: 0.7,
+      evil: 0.8,
+    };
+
+    for (let i = 0; i < 6; i++) {
+      setRandomCellToRandomValue();
+    }
+
+    const solution = this.#solver.solvePuzzle();
+    console.log(solution);
   }
 }
