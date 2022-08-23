7051b356b64fa04beff90016c35cf77fc121551f
exphoenee <61277296+exphoenee@users.noreply.github.com>
2022-08-23 11:48:43 +0200

    Create SudokuGenerator.mjs

---
+++ src/SudokuGenerator/SudokuGenerator.mjs
@@ -0,0 +1,58 @@
+"use strict";
+
+export default class SudokuGenerator {
+  #sudokuboard;
+  #boxSizeX;
+  #boxSizeY;
+  #puzzle;
+
+  constructor(sudokuboard) {
+    //the size of a section and matrix of sections n x n, but the css isn't made for other sizes only 3 x 3 sudokus...
+    this.#boxSizeX = sudokuboard.boardSize.boxSizeX;
+    this.#boxSizeY = sudokuboard.boardSize.boxSizeY;
+    this.#puzzle = sudokuboard.getCellValues({ format: "2D" });
+
+    //using the #sudokuboard calss for handling the sudoku board
+    this.#sudokuboard = sudokuboard;
+  }
+
+  /* gives back the entire sudokuboard
+  arg:    null
+  return  SudokuBoard (Object) */
+  get sudokuboard() {
+    return this.#sudokuboard;
+  }
+
+  /* gives back info form all the cells in the board
+  arg:    null,
+  return: array of object literals */
+  setBoard(puzzle) {
+    this.#sudokuboard.setBoard(puzzle);
+  }
+
+  clearBoard() {
+    this.#sudokuboard.clearBoard();
+  }
+
+  getFreeCells() {
+    return this.#sudokuboard.cells.filter((cell) => cell.isUnfilled());
+  }
+
+  getCellPossiblities(cell) {
+    return this.#sudokuboard.getCellPossiblities(cell);
+  }
+
+  getARandomFreeCell() {
+    const freeCells = this.getFreeCells();
+    return freeCells[Math.floor(Math.random() * freeCells.length)];
+  }
+
+  setARandomCellToRandomValue() {
+    const cell = this.getARandomFreeCell();
+    const possibleities = this.getCellPossiblities(cell);
+    console.log(possibleities);
+    cell.setValue(
+      possibleities[Math.floor(Math.random() * possibleities.length)]
+    );
+  }
+}
